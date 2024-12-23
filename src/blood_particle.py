8a650794c92dfba0de38a1c809327247265e7dff
Elliot <3186037+elliot-100@users.noreply.github.com>
2024-12-23 11:59:36 +0000

    WIP Refactor: more consistent BloodParticle, EnergyOrb attributes and aging handling; delete unused attribute

--- src/blood_particle.py
+++ src/blood_particle.py
@@ -9,7 +9,8 @@ from src.constants import COLORS
 
 class BloodParticle(pygame.sprite.Sprite):
     PARTICLES_PER_SPRAY: ClassVar = 5
-    LIFETIME: ClassVar = 100000
+    LIFETIME: ClassVar = 100 * 1000
+    """Milliseconds."""
 
     def __init__(self, pos: tuple[int, int], angle: float, speed: float) -> None:
         super().__init__()
@@ -18,20 +19,18 @@ class BloodParticle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
         self.dx = speed * math.cos(angle) * -1
         self.dy = speed * math.sin(angle) * -1
-        self.gravity = 0.0
         self.spawn_time = pygame.time.get_ticks()
         self.alpha = 255
 
     def update(self) -> None:
         self.rect.x += self.dx
         self.rect.y += self.dy
-        self.dy += self.gravity
         self.dx *= 0.98
         self.dy *= 0.98
 
-        elapsed_time = pygame.time.get_ticks() - self.spawn_time
-        if elapsed_time < self.LIFETIME:
-            self.alpha = int(255 * (1 - elapsed_time / self.LIFETIME))
-            self.image.set_alpha(self.alpha)
-        else:
+        age = pygame.time.get_ticks() - self.spawn_time
+        if age > self.LIFETIME:
             self.kill()
+        else:
+            self.alpha = int(255 * (1 - age / self.LIFETIME))
+            self.image.set_alpha(self.alpha)
--- src/energy_orb.py
+++ src/energy_orb.py
@@ -1,17 +1,22 @@
+from typing import ClassVar
+
 import pygame
 
 
 class EnergyOrb(pygame.sprite.Sprite):
     """Represents an energy orb that the player can collect."""
 
+    LIFETIME: ClassVar = 10000 * 1000
+    """Milliseconds."""
+
     def __init__(self, x: int, y: int, image: pygame.Surface) -> None:
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect(center=(x, y))
-        self.lifetime = 10000000
         self.spawn_time = pygame.time.get_ticks()
 
     def update(self) -> None:
         """Checks if the orb's lifetime has expired and removes it if so."""
-        if pygame.time.get_ticks() - self.spawn_time > self.lifetime:
+        age = pygame.time.get_ticks() - self.spawn_time
+        if age > self.LIFETIME:
             self.kill()
