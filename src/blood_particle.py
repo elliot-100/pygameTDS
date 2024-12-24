29645e535468b352b3a88d61c620db81f36eb410
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2024-12-24 10:24:58 +0000

    Refactor BloodParticle: delete unused attribute; introduce DECELERATION attribute

--- src/blood_particle.py
+++ src/blood_particle.py
@@ -10,6 +10,8 @@ from src.constants import COLORS
 class BloodParticle(pygame.sprite.Sprite):
     PARTICLES_PER_SPRAY: ClassVar = 5
     LIFETIME: ClassVar = 100000
+    DECELERATION: ClassVar = 0.02
+    """Factor for speed reduction per frame."""
 
     def __init__(self, pos: tuple[int, int], angle: float, speed: float) -> None:
         super().__init__()
@@ -18,16 +20,14 @@ class BloodParticle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=pos)
         self.dx = speed * math.cos(angle) * -1
         self.dy = speed * math.sin(angle) * -1
-        self.gravity = 0.0
         self.spawn_time = pygame.time.get_ticks()
         self.alpha = 255
 
     def update(self) -> None:
         self.rect.x += self.dx
         self.rect.y += self.dy
-        self.dy += self.gravity
-        self.dx *= 0.98
-        self.dy *= 0.98
+        self.dx *= 1 - self.DECELERATION
+        self.dy *= 1 - self.DECELERATION
 
         elapsed_time = pygame.time.get_ticks() - self.spawn_time
         if elapsed_time < self.LIFETIME:
