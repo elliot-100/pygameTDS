f9325c8d3e084501bd96b875a5f28adc7f275c1e
elliot-100 <3186037+elliot-100@users.noreply.github.com>
2024-12-25 13:02:40 +0000

    Refactor BloodParticle: require keyword args; simplify spawning; remove redundant spawn

--- src/blood_particle.py
+++ src/blood_particle.py
@@ -1,6 +1,6 @@
 import math
 import random
-from typing import ClassVar
+from typing import ClassVar, Self
 
 import pygame
 
@@ -10,18 +10,29 @@ from src.constants import COLORS
 class BloodParticle(pygame.sprite.Sprite):
     PARTICLES_PER_SPRAY: ClassVar = 5
     LIFETIME: ClassVar = 100000
+    INITIAL_SPEED_MIN: ClassVar = 0.7
+    """Minimum initial speed, pixels per frame."""
+    INITIAL_SPEED_MAX: ClassVar = 1.5
+    """Maximum initial speed, pixels per frame."""
     DECELERATION: ClassVar = 0.02
     """Factor for speed reduction per frame."""
 
-    def __init__(self, pos: tuple[int, int], angle: float, speed: float) -> None:
+    def __init__(self, *, pos: tuple[int, int]) -> None:
         super().__init__()
         self.image = pygame.Surface((random.randint(1, 5), random.randint(1, 5)))
         self.image.fill(COLORS['RED'])
         self.rect = self.image.get_rect(center=pos)
+        angle = random.uniform(0, 2 * math.pi)
+        speed = random.uniform(self.INITIAL_SPEED_MIN, self.INITIAL_SPEED_MAX)
         self.dx = speed * math.cos(angle) * -1
         self.dy = speed * math.sin(angle) * -1
         self.spawn_time = pygame.time.get_ticks()
         self.alpha = 255
+
+    @classmethod
+    def spawn_spray(cls, *, pos: tuple[int, int]) -> set[Self]:
+        """Return a set of `BloodParticle`s."""
+        return {cls(pos=pos) for _ in range(cls.PARTICLES_PER_SPRAY)}
 
     def update(self) -> None:
         self.rect.x += self.dx
